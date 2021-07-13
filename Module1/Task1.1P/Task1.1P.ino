915b3418a4837c5b14c47c2af39ad32111238fce
Jessie He <hexiaoju@deakin.edu.au>
2021-07-13 22:39:48 +0800

    rewrite task1.1P, use a button instead of PIR. Add a counting job in main loop to observe how does external interrupt work

--- Module1/Task1.1P/Task1.1P.ino
+++ Module1/Task1.1P/Task1.1P.ino
@@ -1,23 +1,41 @@
-// if the PIR sensor detects any motion
-// then the build-in LED will blink
-int val = LOW;
+/**
+ * The LED blinks when the button is pushed
+ * The main loop is busying at counting from 0 to 99
+ * when the button is pushed, the interruptPin sense
+ * its value goes from high to low
+ * so the counting is paused and
+ * the 'blink' function is called.
+ * After the execution of 'blink', 
+ * it goes back to continue counting.
+ * When the button is released, the interruptPin sense
+ * its value goes from low to high, 
+ * so the 'blink' is called again
+ * 
+*/
+const byte ledPin = 7;
+const byte interruptPin = 2;
+volatile byte state = HIGH;
+
+
 void setup()
-{
+{  
   Serial.begin(9600);
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
 }
 
-void loop()
-{
-  val = digitalRead(2);
-  if (val == HIGH)
-      Serial.println("motion detected...");
-  else
-      Serial.println("inactive...");
-      
-  digitalWrite(13, val);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(13, LOW); // turn off the LED anyway to make it blinks
-  delay(500); // Wait for 500 millisecond(s)
+void loop() {
+  for (int i = 0; i < 100; i++)
+  {
+    Serial.println(i);
+    delay(500);
+  }
+}
+
+void blink() {
+  // code that runs when external interrupt is triggered
+  Serial.println("External interrupt");
+  state = !state;
+  digitalWrite(ledPin, state);
 }
