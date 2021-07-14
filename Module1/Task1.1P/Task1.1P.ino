c295c513fcb7d0fad01d82285d165f72c3784cdd
Jessie He <hexiaoju@deakin.edu.au>
2021-07-14 14:26:36 +0800

    1.1 is the normal version, 1.2 is the interrupt version

--- Module1/Task1.1P/Task1.1P.ino
+++ Module1/Task1.1P/Task1.1P.ino
@@ -1,41 +1,36 @@
 /**
- * The LED blinks when the button is pushed
- * The main loop is busying at counting from 0 to 99
- * when the button is pushed, the interruptPin sense
+ * The LED turns on when the button is held
+ * The LED turns off when the button is released
+ * when the button is pushed, the buttonPin sense
  * its value goes from high to low
- * so the counting is paused and
- * the 'blink' function is called.
- * After the execution of 'blink', 
- * it goes back to continue counting.
+ * so the led state will be reversed. (off to on)
  * When the button is released, the interruptPin sense
  * its value goes from low to high, 
- * so the 'blink' is called again
+ * so the led state will be reversed again. (on to off)
  * 
 */
 const byte ledPin = 7;
-const byte interruptPin = 2;
-volatile byte state = HIGH;
+const byte buttonPin = 2;
+volatile byte prevState = HIGH;
 
 
 void setup()
 {  
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  for (int i = 0; i < 100; i++)
-  {
-    Serial.println(i);
-    delay(500);
+  byte state = digitalRead(buttonPin);
+  if (state != prevState) {
+    digitalWrite(ledPin, !state);
+    prevState = state;     
   }
+  Serial.print("input: ");
+  Serial.print(state);
+  Serial.print(" output: ");
+  Serial.println(!state);
+  delay(500);
 }
 
-void blink() {
-  // code that runs when external interrupt is triggered
-  Serial.println("External interrupt");
-  state = !state;
-  digitalWrite(ledPin, state);
-}
--- Module1/Task1.2P/Task1.2P.ino
+++ Module1/Task1.2P/Task1.2P.ino
@@ -1,10 +1,24 @@
-// LED blinks while motion detected by PIR
-// external interrupt version
-const byte ledPin = 13;
+/**
+ * The LED blinks when the button is pushed
+ * The main loop is busying at counting from 0 to 99
+ * when the button is pushed, the interruptPin sense
+ * its value goes from high to low
+ * so the counting is paused and
+ * the 'blink' function is called.
+ * After the execution of 'blink', 
+ * it goes back to continue counting.
+ * When the button is released, the interruptPin sense
+ * its value goes from low to high, 
+ * so the 'blink' is called again
+ * 
+*/
+const byte ledPin = 7;
 const byte interruptPin = 2;
-volatile byte state = LOW;
+volatile byte state = HIGH;
 
-void setup() {
+
+void setup()
+{  
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
@@ -12,22 +26,16 @@ void setup() {
 }
 
 void loop() {
-  if (state == HIGH)
+  for (int i = 0; i < 100; i++)
   {
-    Serial.println("motion detected...");
-    digitalWrite(ledPin, state); // led on
+    Serial.println(i);
     delay(500);
   }
-  else
-  {
-    Serial.println("inactive...");
-  }
-  digitalWrite(ledPin, LOW); // led off
-  delay(500);
-  
 }
 
 void blink() {
+  // code that runs when external interrupt is triggered
+  Serial.println("External interrupt");
   state = !state;
-  
+  digitalWrite(ledPin, state);
 }
