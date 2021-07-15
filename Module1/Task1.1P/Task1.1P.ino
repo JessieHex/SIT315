1b9c1072bbbbe448e525fe6f695d608ade3efc67
JessieHex <84708253+JessieHex@users.noreply.github.com>
2021-07-15 17:26:00 +0800

    Correct the comment

--- Module1/Task1.1P/Task1.1P.ino
+++ Module1/Task1.1P/Task1.1P.ino
@@ -1,11 +1,11 @@
 /**
  * The LED turns on when the button is held
  * The LED turns off when the button is released
- * when the button is pushed, the buttonPin sense
- * its value goes from high to low
+ * when the button is pushed,
+ * its state goes from high to low
  * so the led state will be reversed. (off to on)
- * When the button is released, the interruptPin sense
- * its value goes from low to high, 
+ * When the button is released,  
+ * its state goes from low to high, 
  * so the led state will be reversed again. (on to off)
  * 
 */
